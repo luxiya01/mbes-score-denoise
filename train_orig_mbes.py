e12587aa5709c06c63dacdb9e002e0a97c231d8f
Li Ling <liling@kth.se>
2024-07-02 13:25:28 +0200

    Training: add param for global Z normalization

--- train_orig_mbes.py
+++ train_orig_mbes.py
@@ -27,6 +27,8 @@ parser.add_argument('--gt_root', type=str, required=True)
 parser.add_argument('--noise_min', type=float, default=0.005)
 parser.add_argument('--noise_max', type=float, default=0.020)
 parser.add_argument('--train_batch_size', type=int, default=32)
+parser.add_argument('--global_norm_z', action='store_true',
+                    help='If True, normalize Z globally using pre-computed max z per data split.')
 # parser.add_argument('--val_batch_size', type=int, default=64)
 parser.add_argument('--num_workers', type=int, default=4)
 parser.add_argument('--aug_rotate', type=eval, default=True, choices=[True, False])
@@ -82,6 +84,21 @@ logger.info(args)
 # Datasets and loaders
 logger.info('Loading datasets')
 
+global_norm_z = {
+    'train': 45.53,
+    'validation': 31.44,
+    'test': 28.82,
+}
+instance_norm_z = {
+    'train': None,
+    'validation': None,
+    'test': None,
+}
+if args.global_norm_z:
+    norm_z_dict = global_norm_z
+else:
+    norm_z_dict = instance_norm_z
+
 def load_training_data():
     train_dset = MBESDataset(
         use_ping_idx=False,
@@ -89,7 +106,7 @@ def load_training_data():
         gt_root=args.gt_root,
         split='train_data_merged',
         transform=Compose([
-            NormalizeZ(z_scale=45.53),
+            NormalizeZ(z_scale=norm_z_dict['train']),
             RandomRotate(axis=2),
         ])
     )
@@ -101,7 +118,7 @@ def load_training_data():
         gt_root=args.gt_root,
         split='validation',
         transform=Compose([
-            NormalizeZ(z_scale=31.44),
+            NormalizeZ(z_scale=norm_z_dict['validation']),
             RandomRotate(axis=2),
         ])
     )
@@ -115,7 +132,7 @@ def load_test_data():
         gt_root=args.gt_root,
         split='test_data.npz',
         transform=Compose([
-            NormalizeZ(z_scale=28.82),
+            NormalizeZ(z_scale=norm_z_dict['test']),
         ])
     )
     return test_dset
@@ -186,19 +203,24 @@ def validate(it, dset, store_results=False, ckpt_path=None):
     if store_results:
         if ckpt_path is None:
             raise ValueError('ckpt_path must be provided if store_results is True')
-        os.makedirs(f'{ckpt_path}_results', exist_ok=True)
+        if args.global_norm_z:
+            norm_str = 'global'
+        else:
+            norm_str = 'instance'
+        save_dir = f'{ckpt_path}_results_{norm_str}_norm'
+        os.makedirs(save_dir, exist_ok=False)
         
 
     for i, data in enumerate(tqdm(dset, desc='Validate')):
-        if i > 10:
-            break
-
         pcl_noisy = data['pcl_noisy'].reshape(-1, 3).to(args.device)
         pcl_clean = data['pcl_clean'].reshape(-1, 3).to(args.device)
         gt_rejected_mask = data['rejected'].to(args.device) # Shape: (N,)
         valid_mask = data['valid_mask'].to(args.device)
         scale_xy = data['scale_xy'].to(args.device)
-        scale_z = data['scale_z']#.to(args.device)
+        if args.global_norm_z:
+            scale_z = data['scale_z']
+        else:
+            scale_z = data['scale_z'].to(args.device)
         center = data['center'].to(args.device)
 
         pcl_denoised, init_grad = patch_based_denoise(model, pcl_noisy, ld_step_size=args.ld_step_size,
@@ -206,7 +228,7 @@ def validate(it, dset, store_results=False, ckpt_path=None):
         denoising_results = compute_mbes_denoising_metrics(pcl_denoised, pcl_clean, valid_mask=valid_mask, denormalize=True,
                                               scale_xy=scale_xy, scale_z=scale_z, center=center)
         outlier_results = compute_mbes_outlier_rejection_metrics(init_grad.flatten(), gt_rejected_mask.flatten(),
-                                                                thresh=1.5)
+                                                                thresh=5.)
 
         for k, v in denoising_results.items():
             denoising_metrics_denorm[k].append(v)
@@ -222,34 +244,38 @@ def validate(it, dset, store_results=False, ckpt_path=None):
             gt_rejected_mask = gt_rejected_mask.cpu().numpy()
             valid_mask = valid_mask.cpu().numpy()
             scale_xy = scale_xy.cpu().numpy()
-            scale_z = scale_z.cpu().numpy()
+            if args.global_norm_z:
+                scale_z = scale_z
+            else:
+                scale_z = scale_z.cpu().numpy()
             center = center.cpu().numpy()
-            np.savez(f'{ckpt_path}_results/patch_based_denoise_{i}_patchid_{idx}.npz', pcl_denoised=pcl_denoised, pcl_clean=pcl_clean,
+            np.savez(f'{save_dir}/patch_based_denoise_{i}_patchid_{idx}.npz', pcl_denoised=pcl_denoised, pcl_clean=pcl_clean,
                      pcl_noisy=pcl_noisy, init_grad=init_grad, gt_rejected_mask=gt_rejected_mask, valid_mask=valid_mask,
                      scale_xy=scale_xy, scale_z=scale_z, center=center)
 
     avg_metrics = {f'{k}_denorm': np.mean(v) for k, v in denoising_metrics_denorm.items()}
+    avg_metrics.update({
+        f'{k}_sum': np.sum(v) for k, v in binary_metrics.items()
+    })
     
     # compute accuracy, precision, recall, F1-score from binary_metrics using the _sum values
-    TP_sum = np.sum(binary_metrics['TP'])
-    FP_sum = np.sum(binary_metrics['FP'])
-    TN_sum = np.sum(binary_metrics['TN'])
-    FN_sum = np.sum(binary_metrics['FN'])
-    binary_metrics['accuracy'] = (TP_sum + TN_sum) / (TP_sum + FP_sum + TN_sum + FN_sum)
-    binary_metrics['precision'] = TP_sum / (TP_sum + FP_sum)
-    binary_metrics['recall'] = TP_sum / (TP_sum + FN_sum)
+    TP_sum = avg_metrics['TP_sum']
+    FP_sum = avg_metrics['FP_sum']
+    TN_sum = avg_metrics['TN_sum']
+    FN_sum = avg_metrics['FN_sum']
+    avg_metrics['accuracy'] = (TP_sum + TN_sum) / (TP_sum + FP_sum + TN_sum + FN_sum)
+    avg_metrics['precision'] = TP_sum / (TP_sum + FP_sum)
+    avg_metrics['recall'] = TP_sum / (TP_sum + FN_sum)
     # compute F1-score
-    binary_metrics['f1_score'] = 2 * (binary_metrics['precision'] * binary_metrics['recall']) / (binary_metrics['precision'] + binary_metrics['recall'])
+    avg_metrics['f1_score'] = 2 * (avg_metrics['precision'] * avg_metrics['recall']) / (avg_metrics['precision'] + avg_metrics['recall'])
 
     # logger.info('[Val] Iter %04d | CD %.6f  ' % (it, avg_chamfer))
     # writer.add_scalar('val/chamfer', avg_chamfer, it)
     logger.info(f'[Val] Iter {it:4d} Denoise | CD {avg_metrics["cd_denorm"]:.6f} | |z| {avg_metrics["z_abs_diff_denorm"]:.6f} | z_rmse {avg_metrics["z_rmse_denorm"]:.6f}')
     # log accuracy, precision, recall, F1-score
-    logger.info(f'[Val] Iter {it:4d} Outlier | Accuracy {binary_metrics["accuracy"]*100:.2f}% | Precision {binary_metrics["precision"]*100:.2f}% | Recall {binary_metrics["recall"]*100:.2f}% | F1-score {binary_metrics["f1_score"]:.6f}')
+    logger.info(f'[Val] Iter {it:4d} Outlier | Accuracy {avg_metrics["accuracy"]*100:.2f}% | Precision {avg_metrics["precision"]*100:.2f}% | Recall {avg_metrics["recall"]*100:.2f}% | F1-score {avg_metrics["f1_score"]:.6f}')
     logger.info(f'[Val] Iter {it:4d} Outlier | TP {TP_sum} | FP {FP_sum} | TN {TN_sum} | FN {FN_sum}')
     for k, v in avg_metrics.items():
-        writer.add_scalar(f'val/{k}', v, it)
-    for k, v in binary_metrics.items():
         writer.add_scalar(f'val/{k}', v, it)
     writer.flush()
 
