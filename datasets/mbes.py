08147ecfdc04ffd1cd16aacd38e4b2771b8c1ed0
Li Ling <liling@kth.se>
2024-07-16 11:41:12 +0200

    MBES dataset: implement ping, beam, range representation

--- datasets/mbes.py
+++ datasets/mbes.py
@@ -23,31 +23,30 @@ class MBESDataset(Dataset):
         self.transform = transform
         self.use_ping_idx = use_ping_idx
 
-    def _replace_xy_with_ping_beam_idx(self, pcl):
+    def _use_ping_beam_range_representation(self, pcl, angles):
         num_pings, num_beams = pcl.shape[:2]
         ping_idx = np.arange(num_pings).reshape(-1, 1).repeat(num_beams, axis=1)
         beam_idx = np.arange(num_beams).reshape(1, -1).repeat(num_pings, axis=0)
+        z_range = pcl[:, :, 2] / np.cos(np.radians(angles))
         pcl[:, :, 0] = ping_idx
         pcl[:, :, 1] = beam_idx
+        pcl[:, :, 2] = z_range
         return pcl
 
     def _load_raw_data(self, idx):
         data = np.load(self.raw_data_files[idx], allow_pickle=True)
+        angles = data["angle"]
         pcl = np.stack([data["X"], data["Y"], data["Z_relative"]], axis=-1)
-        if self.use_ping_idx:
-            pcl = self._replace_xy_with_ping_beam_idx(pcl)
         pcl = pcl.reshape(-1, 3)
 
         # Load the rejection mask and record the indices of rejected points, indexed as 1D array
         rejected = np.argwhere(data["rejected"].flatten(), ).flatten()
-        return pcl, rejected
+        return pcl, rejected, angles
 
     def _load_gt_data(self, idx):
         data = np.load(self.gt_data_files[idx], allow_pickle=True)
         # filter out invalid draping points
         pcl = data['data']
-        if self.use_ping_idx:
-            pcl = self._replace_xy_with_ping_beam_idx(pcl)
         valid_mask = data['valid_mask']
         pcl = pcl[valid_mask]
         return pcl, valid_mask
@@ -56,8 +55,11 @@ class MBESDataset(Dataset):
         return len(self.raw_data_files)
 
     def __getitem__(self, idx):
-        pcl_raw, rejected = self._load_raw_data(idx)
+        pcl_raw, rejected, angles = self._load_raw_data(idx)
         pcl_gt, valid_mask = self._load_gt_data(idx)
+        if self.use_ping_idx:
+            pcl_raw = self._use_ping_beam_range_representation(pcl_raw, angles)
+            pcl_gt = self._use_ping_beam_range_representation(pcl_gt, angles)
 
         data = {
             'pcl_clean': torch.from_numpy(pcl_gt).clone().float(),
